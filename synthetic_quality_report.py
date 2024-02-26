770dc4d9c0749fa92830c7916a55f99c0b12d742
Muhammad Mobeen <70343951+muhammad-mobeen@users.noreply.github.com>
2024-02-26 23:30:53 +0500

    Update synthetic_quality_report.py

--- synthetic_quality_report.py
+++ synthetic_quality_report.py
@@ -5,9 +5,12 @@ import json
 import os
 
 class SyntheticQualityAssurance:
-    def __init__(self, original_file_path, synthetic_file_path) -> None:
+    def __init__(self, original_file_path, synthetic_file_path, model="ctgan") -> None:
         self.data_df = pd.read_csv(original_file_path)
         self.synthetic_data_df = pd.read_csv(synthetic_file_path)
+        if model == "dgan":        # Metadata not happy with 'example_id' column
+            if 'example_id' in self.synthetic_data_df.columns:
+                self.synthetic_data_df.drop(['example_id'], axis = 1, inplace = True) 
         self.metadata = SingleTableMetadata()
         self.metadata.detect_from_dataframe(self.data_df)
         self.metadata = self.metadata.to_dict()
